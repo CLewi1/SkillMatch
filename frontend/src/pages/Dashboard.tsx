d7bc8a7341905ac4dac48f63daee10280e57cd1e
Colin Lewandowski <75598914+CLewi1@users.noreply.github.com>
2025-08-08 19:23:04 +0000

    Phase 05 - Styling changes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 function Dashboard() {
 
     return(
-    <div className="border-b-2 border-white ">
-        <h1 className="text-4xl font-bold">Dashboard</h1>
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
+        <h1 className="text-4xl font-bold text-black">Dashboard</h1>
 
     </div>
     );
